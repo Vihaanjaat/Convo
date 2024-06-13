1b499dce3c3d6eca793f14f35b5d9bf4ebf08451
AsmitX <162042032+AsmitX@users.noreply.github.com>
2024-06-13 16:10:29 +0545

    Update convo.py

--- convo.py
+++ convo.py
@@ -1,9 +1,8 @@
 import os,platform
-os.system("xdg-open https://www.facebook.com/677366316")
 os.system('git pull')
 
 convo=platform.architecture()[0]
-if trt=="32bit":
+if convo=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif convo=="64bit":
     __import__("puti")
